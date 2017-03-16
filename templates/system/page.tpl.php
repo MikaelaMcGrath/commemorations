70ba3b7a1977241db6323bd444581265e77519a2
Mikaela McGrath <mikaelamcgrath@hotmail.com>
2017-03-16 15:22:59 +1100

    Removed title and breadcrumbs from template

--- templates/system/page.tpl.php
+++ templates/system/page.tpl.php
@@ -108,14 +108,7 @@
 
   <div class="l-main">
     <div class="l-content" role="main">
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
       <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
       <?php print $messages; ?>
       <?php print render($tabs); ?>
       <?php print render($page['help']); ?>
