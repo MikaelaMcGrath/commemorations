e51739257b90c8f7b09bda549af58b1e1d98f00c
Mikaela McGrath <mikaelamcgrath@hotmail.com>
2017-05-12 19:10:42 +1000

    jQuery to add class when text is first paragraph type and styled content

--- js/commemorations.behaviors.js
+++ js/commemorations.behaviors.js
@@ -451,6 +451,19 @@ Drupal.behaviors.commemorationsReadMore = {
       });
     }
   };
+  Drupal.behaviors.commemorationsSummaryRelatedContent = {
+  attach: function (context, settings) {
+      // By using the 'context' variable we make sure that our code only runs on
+      // the relevant HTML. Furthermore, by using jQuery.once() we make sure that
+      // we don't run the same piece of code for an HTML snippet that we already
+      // processed previously. By using .once('foo') all processed elements will
+      // get tagged with a 'foo-processed' class, causing all future invocations
+      // of this behavior to ignore them.
+      $('.related-content', context).once('summaryRelatedContent', function () {
+        $('div.field__items>.field__item').find('.group-summary-text:first-child').parent().addClass('summary-related-content');
+      });
+    }
+  };
   
 })(jQuery);
 
