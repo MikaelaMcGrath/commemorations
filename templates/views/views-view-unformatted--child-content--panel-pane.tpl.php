9019e7e9c01167ed458647d9efbc838edc7c5af4
Mikaela McGrath <mikaelamcgrath@hotmail.com>
2017-06-13 21:54:50 +1000

    Resolved PHP error on template

--- templates/views/views-view-unformatted--child-content--panel-pane.tpl.php
+++ templates/views/views-view-unformatted--child-content--panel-pane.tpl.php
@@ -10,12 +10,9 @@
 <?php if (!empty($title)): ?>
   <?php print $title; ?>
 <?php endif; ?>
-
-<?php $count = 0; ?>
 <?php foreach ($rows as $id => $row): ?>
   <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
     <?php print $row; ?>
   </div>
-
-
+<?php endforeach; ?>
 
