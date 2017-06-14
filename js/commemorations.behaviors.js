417fcea2a9ba01291f6ae3f6547c410578ae6cd4
Mikaela McGrath <mikaelamcgrath@hotmail.com>
2017-06-15 09:32:12 +1000

    Changed VC map class

--- js/commemorations.behaviors.js
+++ js/commemorations.behaviors.js
@@ -77,15 +77,15 @@ Drupal.behaviors.commemorationsReadMore = {
             $(this).attr('value', 'Expand').next().slideUp();
           }
         });
-        $('.leaflet-container').slideUp().addClass('collapsed');
+        $('.vc-map').slideUp().addClass('collapsed');
         $('.vc-body .map-more').addClass('collapsed');
         $('.vc-body .map-more').click(function() {
           if($(this).hasClass('collapsed')) {
-            $(this).parent('.vc-body').next('.leaflet-container').slideDown().addClass('expanded');
+            $(this).parent('.vc-body').next('.vc-map').slideDown().addClass('expanded');
             $(this).addClass('expanded').removeClass('collapsed').html('<a>Hide map</a>');
           }
           else {
-            $(this).parent('.vc-body').next('.leaflet-container').slideUp().addClass('collapsed');
+            $(this).parent('.vc-body').next('.vc-map').slideUp().addClass('collapsed');
             $(this).addClass('collapsed').removeClass('expanded').html('<a>View map</a>');
           }
         });
