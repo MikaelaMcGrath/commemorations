007ba0cabdeaae67de6524c7eddc3838a396b383
Mikaela McGrath <mikaelamcgrath@hotmail.com>
2017-06-15 08:59:36 +1000

    Initial education style and jQuery for show/more

--- js/commemorations.behaviors.js
+++ js/commemorations.behaviors.js
@@ -161,6 +161,33 @@ Drupal.behaviors.commemorationsReadMore = {
       //$('.view-id-child_content .view-content', context).once('viewMore', function () {
         //$('.more-link').replaceWith($('.views-row-1 .menu-path'));
       //});
+      $('.education', context).once('ed', function () {
+        var showChar = 256;
+        var ellipsestext = "...";
+        var moretext = "See More";
+        var lesstext = "See Less";
+        $('.education-body').each(function () {
+            var content = $(this).html();
+            if (content.length > showChar) {
+                var show_content = content.substr(0, showChar);
+                var hide_content = content.substr(showChar, content.length - showChar);
+                var html = show_content + '<span class="moreelipses">' + ellipsestext + '</span><span class="remaining-content"><span>' + hide_content + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+                $(this).html(html);
+            }
+        });
+
+        $(".morelink").click(function () {
+            if ($(this).hasClass("less")) {
+                $(this).removeClass("less");
+                $(this).html(moretext);
+            } else {
+                $(this).addClass("less");
+                $(this).html(lesstext);
+            }
+            $(this).parent().prev().toggle();
+            
+        });
+      });
     }
   };
 })(jQuery);
