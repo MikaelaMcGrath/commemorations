ea7a70d39fd4287177616e290fcd2b8c66358e71
Mikaela McGrath <mikaelamcgrath@hotmail.com>
2017-08-09 14:26:50 +1000

    jQuery added for removing spacing for video galleries with only one video

--- js/commemorations.behaviors.js
+++ js/commemorations.behaviors.js
@@ -211,6 +211,18 @@ Drupal.behaviors.commemorationsReadMore = {
       });
     }
   };
-
+   Drupal.behaviors.ccommemorationsVideoGallery = {
+    attach: function (context, settings) {
+      // By using the 'context' variable we make sure that our code only runs on
+      // the relevant HTML. Furthermore, by using jQuery.once() we make sure that
+      // we don't run the same piece of code for an HTML snippet that we already
+      // processed previously. By using .once('foo') all processed elements will
+      // get tagged with a 'foo-processed' class, causing all future invocations
+      // of this behavior to ignore them.
+      $('.video-gallery', context).once('multiple-videos', function () {
+        $(this).find('.views-slideshow-controls-bottom').prev().children('.views-slideshow-cycle-main-frame').addClass('multi-gallery');
+      });
+    }
+  };
 })(jQuery);
 
